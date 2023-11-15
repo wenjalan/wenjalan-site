413794115df31c49bc68249a2eef67fb140a437e
Alan Wen <wenjalan@gmail.com>
2023-11-14 16:13:34 -0800

    Load content from markdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,82 @@
+'use client'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import Markdown from 'react-markdown'
+
 export default function Home() {
   return (
-    <>
+    <div className='flex flex-col'>
       <NavBar />
       <Main />
-    </>
+    </div>
   )
 }
 
 function NavBar() {
   return (
     <nav className="bg-slate-50 p-2 flex flex-row">
-      <div className="font-bold px-2">wenjalan</div>
+      <Link href="/" className="rounded-md hover:bg-slate-300 font-bold px-2">wenjalan</Link>
     </nav>
   )
 }
 
 function Main() {
+  const [sections, setSections] = useState([])
+  const [selected, setSelected] = useState('about') // default to about.md before metadata is loaded
+
+  useEffect(() => {
+    fetch('./metadata.json')
+      .then((res) => {
+        return res.ok ? res.json() : ['Something went wrong.']
+      })
+      .then((metadata) => {
+        setSections(metadata.sections)
+        setSelected(metadata.sections[0])
+      })
+  }, [])
+
   return (
     <main className="m-4 flex flex-row gap-2">
-      <Menu />
-      <Content />
+      <Menu sections={sections} selected={selected} setSelected={setSelected} />
+      <Content src={`./${selected}.md`} />
     </main>
   )
 }
 
-function Menu() {
+interface MenuProps {
+  sections: string[]
+  selected: string
+  setSelected: (page: string) => void
+}
+
+function Menu(props: MenuProps) {
   return (
-    <div className="text-1xl font-bold flex-4 flex flex-col gap-2">
-      <div className="p-4 rounded-md bg-slate-50">About Me</div>
-      <div className="p-4 rounded-md bg-slate-50">Education</div>
-      <div className="p-4 rounded-md bg-slate-50">Experience</div>
-      <div className="p-4 rounded-md bg-slate-50">Projects</div>
-      <div className="p-4 rounded-md bg-slate-50">Awards & Certifications</div>
+    <div className="text-1xl flex-4 flex flex-col gap-2">
+      {props.sections.map((section) => (
+        section === props.selected ?
+          <div key={section} className="hover:bg-slate-300 cursor-pointer font-bold p-4 rounded-md bg-slate-50" onClick={() => props.setSelected(section)}>{section.toUpperCase()}</div> :
+          <div key={section} className="hover:bg-slate-300 cursor-pointer p-4 rounded-md bg-slate-50" onClick={() => props.setSelected(section)}>{section.toUpperCase()}</div>
+      ))}
     </div>
   )
 }
 
-function Content() {
+interface ContentProps {
+  src: string
+}
+
+function Content(props: ContentProps) {
+  const [content, setContent] = useState('Loading...')
+
+  useEffect(() => {
+    fetch(props.src)
+      .then((res) => {
+        return res.ok ? res.text() : 'Something went wrong.'
+      })
+      .then((content) => setContent(content))
+  }, [props.src])
+
   return (
-    <div className=" p-4 bg-slate-50 rounded-md flex-1">
-      <b>About Me</b>
-      <p>
-        I am a software engineer with 13 years of experience in programming.
-      </p>
-    </div>
+    <Markdown className="p-4 bg-slate-50 rounded-md flex-1">{content}</Markdown>
   )
 }
---
+++ public/about.md
@@ -0,0 +1,14 @@
+**About Me**  
+I am a software engineer with 13 years of programming experience.  
+I am a software engineer with 13 years of programming experience.  
+I am a software engineer with 13 years of programming experience.  
+I am a software engineer with 13 years of programming experience.  
+I am a software engineer with 13 years of programming experience.  
+I am a software engineer with 13 years of programming experience.  
+I am a software engineer with 13 years of programming experience.  
+I am a software engineer with 13 years of programming experience.  
+I am a software engineer with 13 years of programming experience.  
+I am a software engineer with 13 years of programming experience.  
+I am a software engineer with 13 years of programming experience.  
+I am a software engineer with 13 years of programming experience.  
+I am a software engineer with 13 years of programming experience.  
