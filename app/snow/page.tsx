e30bc4b9bb92a5a1842021ea037a25f818428b25
Alan Wen <wenjalan@gmail.com>
2023-11-28 17:06:13 -0800

    Panels update

--- app/snow/page.tsx
+++ app/snow/page.tsx
@@ -20,7 +20,9 @@ export default function Snow() {
 
 interface ResortMetadata {
   name: string,
+  url: string,
   statusUrl: string,
+  weatherUrl: string,
   weatherDataUrl: string,
   snowForecastUrl: string,
 }
@@ -36,21 +38,24 @@ function Main() {
   const resorts = RESORTS as ResortMetadata[]
 
   return (
-    <main className="m-2 p-2 bg-slate-800 flex flex-col gap-2 sm:max-w-xl text-white drop-shadow-lg">
+    <main className="m-2 p-2 bg-slate-800 flex flex-col gap-2 text-white drop-shadow-lg">
       <h1 className="font-bold text-xl">Snow Report</h1>
       <p className="text-lg">Collects temperature, snowfall, lift status and trail status from various ski resorts.</p>
       <i>First 3 numbers are the low, current, and high temperatures. The next 3 are snowfall in the next 24, next 3 days, and next week.</i>
-      {
-        resorts.length === 0 ?
-          "Loading..." :
-          resorts.map(resort => <Resort key={resort.name} resortMetadata={resort} />)
-      }
+      <div className="flex flex-row flex-wrap gap-2">
+        {
+          resorts.length === 0 ?
+            "Loading..." :
+            resorts.map(resort => <Resort key={resort.name} resortMetadata={resort} />)
+        }
+      </div>
     </main>
   )
 }
 
 function Resort(props: { resortMetadata: ResortMetadata }) {
   const [resort, setResort] = useState<ResortData | null>(null)
+  const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
     if (!resort) {
@@ -63,18 +68,20 @@ function Resort(props: { resortMetadata: ResortMetadata }) {
         const terrain = values[1]
         const snow = values[2]
         setResort({ name: props.resortMetadata.name, weather, terrain, snow })
+      }).catch(e => {
+        setError(e.message)
       })
     }
   }, [resort, props.resortMetadata])
 
   if (!resort) {
     return (
-      <div className="p-2 bg-slate-700 flex flex-col gap-2 drop-shadow-lg">
-        <span className="flex flex-row">
-          <h1 className="flex-1 font-bold text-xl">{props.resortMetadata.name}</h1>
-          <span className="flex-1 text-end text-xl">Loading...</span>
+      <div className="p-2 bg-slate-700 flex flex-col gap-2 drop-shadow-lg rounded">
+        <h1 className="flex-1 font-bold text-xl text-slate-800"><a href={props.resortMetadata.url} target="_blank">{props.resortMetadata.name}</a></h1>
+        <span className="flex-1 text-center">
+          {error ? <p><EmojiIcon emoji="⚠️" />Error: {error}</p> : <EmojiIcon emoji="🔍" />}
         </span>
-      </div>
+      </div >
     )
   }
 
@@ -90,56 +97,85 @@ function Resort(props: { resortMetadata: ResortMetadata }) {
   const snowNext7d_cm = resort.snow.forecast.reduce((a, b) => a + b, 0)
   const snowNext7d_in = Math.round(snowNext7d_cm / 2.54)
 
+
   return (
-    <div className="p-2 bg-slate-700 flex flex-col gap-2 drop-shadow-lg">
-      <span className="flex flex-row">
-        <h1 className="flex-1 font-bold text-xl">{resort.name}</h1>
-        <span className="flex-1 text-end font-bold text-xl">❄️ {snowNext7d_in}&quot; {weatherToEmoji(resort.weather.weather)} {resort.weather.tempCurrent}°F</span>
+    <div className="p-2 bg-slate-700 flex flex-col gap-2 drop-shadow-lg rounded">
+      <h1 className="font-bold text-xl text-center"><a href={props.resortMetadata.url} target="_blank">{props.resortMetadata.name}</a></h1>
+      <span className="flex flex-row text-white gap-1 text-sm leading-10 text-center">
+        <EmojiIcon emoji="🌡️" />
+        <TempIndicator temp={resort.weather.tempLow} srcUrl={props.resortMetadata.weatherUrl} />
+        <TempIndicator temp={resort.weather.tempCurrent} srcUrl={props.resortMetadata.weatherUrl} />
+        <TempIndicator temp={resort.weather.tempHigh} srcUrl={props.resortMetadata.weatherUrl} />
       </span>
-      <span className="flex flex-row text-black gap-2">
-        <TempIndicator temp={resort.weather.tempLow} />
-        <TempIndicator temp={resort.weather.tempCurrent} />
-        <TempIndicator temp={resort.weather.tempHigh} />
-        <SnowIndicator inches={snowNext24h_in} />
-        <SnowIndicator inches={snowNext3d_in} />
-        <SnowIndicator inches={snowNext7d_in} />
+      <span className="flex flex-row text-white gap-1 text-sm leading-10 text-center">
+        <EmojiIcon emoji="❄️ " />
+        <SnowIndicator inches={snowNext24h_in} srcUrl={props.resortMetadata.snowForecastUrl} />
+        <SnowIndicator inches={snowNext3d_in} srcUrl={props.resortMetadata.snowForecastUrl} />
+        <SnowIndicator inches={snowNext7d_in} srcUrl={props.resortMetadata.snowForecastUrl} />
+      </span>
+      <span className="flex flex-row text-white gap-1 text-sm leading-10 text-center">
+        <EmojiIcon emoji="🚡" />
+        <PercentIndicator percent={resort.terrain.terrainOpenPercent} srcUrl={props.resortMetadata.statusUrl} />
       </span>
     </div>
   )
 }
 
-function TempIndicator(props: { temp: number }) {
+function EmojiIcon(props: { emoji: string }) {
+  return <span className="text-2xl leading-10 w-10 h-10">{props.emoji}</span>
+}
+
+function PercentIndicator(props: { percent: number, srcUrl: string }) {
+  const p = props.percent
+  const display = p + "%"
+  if (p === 100) {
+    return <span className="cursor-pointer bg-green-700 w-10 h-10 rounded" onClick={(e) => open(props.srcUrl)}>{display}</span>
+  }
+  if (p > 90) {
+    return <span className="cursor-pointer bg-emerald-700 w-10 h-10 rounded" onClick={(e) => open(props.srcUrl)}>{display}</span>
+  }
+  if (p > 50) {
+    return <span className="cursor-pointer bg-amber-700 w-10 h-10 rounded" onClick={(e) => open(props.srcUrl)}>{display}</span>
+  }
+  if (p > 10) {
+    return <span className="cursor-pointer bg-orange-700 w-10 h-10 rounded" onClick={(e) => open(props.srcUrl)}>{display}</span>
+  }
+  return <span className="cursor-pointer bg-red-700 w-10 h-10 rounded" onClick={(e) => open(props.srcUrl)}>{display}</span>
+}
+
+function TempIndicator(props: { temp: number, srcUrl: string }) {
   const t = props.temp
   const display = t + "°"
   if (t > 32) {
-    return <span className="bg-emerald-500 w-10 h-10 text-center font-bold text-lg leading-10 rounded-md">{display}</span>
+    return <span className="cursor-pointer bg-green-600 w-10 h-10 rounded" onClick={(e) => open(props.srcUrl)}>{display}</span>
   }
   if (t > 20) {
-    return <span className="bg-cyan-500 w-10 h-10 text-center font-bold text-lg leading-10 rounded-md">{display}</span>
+    return <span className="cursor-pointer bg-cyan-600 w-10 h-10 rounded" onClick={(e) => open(props.srcUrl)}>{display}</span>
   }
   if (t > 10) {
-    return <span className="bg-blue-500 w-10 h-10 text-center font-bold text-lg leading-10 rounded-md">{display}</span>
+    return <span className="cursor-pointer bg-blue-600 w-10 h-10 rounded" onClick={(e) => open(props.srcUrl)}>{display}</span>
   }
   if (t > 0) {
-    return <span className="bg-indigo-500 w-10 h-10 text-center font-bold text-lg leading-10 rounded-md">{display}</span>
+    return <span className="cursor-pointer bg-indigo-700 w-10 h-10 rounded" onClick={(e) => open(props.srcUrl)}>{display}</span>
   }
   else {
-    return <span className="bg-violet-500 w-10 h-10 text-center font-bold text-lg leading-10 rounded-md">{display}</span>
+    return <span className="cursor-pointer bg-violet-700 w-10 h-10 rounded" onClick={(e) => open(props.srcUrl)}>{display}</span>
   }
 }
 
-function SnowIndicator(props: { inches: number }) {
+function SnowIndicator(props: { inches: number, srcUrl: string }) {
   const i = props.inches
+  const display = i === 0 ? "-" : i + "\""
 
+  // red = [15, sqrt(i) * 15, 100]
+  const red = Math.max(Math.min(Math.sqrt(i) * 15, 100), 15)
+  const green = Math.min(red / 2, 5)
+  const blue = Math.min(red / 2, 10)
+  const color = `rgb(${red}%, ${green}%, ${blue}%)`
 
-  // color is red, saturation depends on inches where 0 = black and 18 = fully red
-  const red = ((i / 18) * 100)
-  const green = red / 8
-  const color = `rgb(${red}%, ${green}%, 0%)`
-
-  return <span className="w-10 h-10 text-center font-bold text-lg leading-10 rounded-md text-white" style={{
+  return <span className="cursor-pointer w-10 h-10 text-white rounded" onClick={(e) => open(props.srcUrl)} style={{
     backgroundColor: color,
-  }}>{i}&quot;</span>
+  }}>{display}</span>
 }
 
 function weatherToEmoji(weather: ResortWeatherData["weather"]): string {
@@ -167,6 +203,7 @@ async function getWeatherData(url: string): Promise<ResortWeatherData> {
       'Access-Control-Allow-Origin': '*'
     },
   })
+  if (!res.ok) throw new Error(`${res.status} ${res.statusText}`)
   return await res.json()
 }
 
@@ -178,6 +215,7 @@ async function getTerrainData(url: string): Promise<ResortTerrainLiftData> {
       'Access-Control-Allow-Origin': '*'
     },
   })
+  if (!res.ok) throw new Error(`${res.status} ${res.statusText}`)
   return await res.json()
 }
 
@@ -189,6 +227,7 @@ async function getSnowData(url: string): Promise<ResortForecastData> {
       'Access-Control-Allow-Origin': '*'
     },
   })
+  if (!res.ok) throw new Error(`${res.status} ${res.statusText}`)
   return await res.json()
 }
 
